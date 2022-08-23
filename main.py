9e99a0741e49deb99fbc7185a2e778bb7a78b90e
nemalipurianurag <63407647+nemalipurianurag@users.noreply.github.com>
2022-08-23 14:44:44 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+last_etag = '0'
+
+while True:
+    r = requests.get(
+        url,
+        params={'last_etag': last_etag, 'wait_for_change': True},
+        headers=METADATA_HEADERS)
+
+    # During maintenance the service can return a 503, so these should
+    # be retried.
+    if r.status_code == 503:
+        time.sleep(1)
+        continue
+    r.raise_for_status()
+
+    last_etag = r.headers['etag']
